ebfee2cb4e369015f6a549c9ded892c63c06f866
Adepoju Abiodun <adepojufatai@gmail.com>
2025-08-26 11:45:59 +0100

    fix vite base config

---
+++ qr-code-generator/vite.config.js
@@ -0,0 +1,6 @@
+// qr-code-generator/vite.config.js
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: './', // 👈 ensures assets like CSS/JS are loaded correctly in dist
+})
